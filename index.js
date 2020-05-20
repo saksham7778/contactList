c9ac570ff076465eeccadeb7a08cbb1aae0d497f
saksham7778 <itzmesaksham@gmail.com>
2020-05-20 21:34:58 +0530

    view and add contact added in static list

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
 const express= require('express');
 const port =8000;
+const path = require('path');
 
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views' , path.join(__dirname,'views'));
+
+//middleware 1 => for encoding  
+app.use(express.urlencoded());
+//middleware 2  ==> for styling
+app.use(express.static('assets'));
+
+//static kind of database
+var contactList =[
+    {
+        name : 'saksham',
+        phone: '1234567890'
+    },
+
+    {
+        name : 'hello',
+        phone: '1111111111'
+    },
+
+    {
+        name : 'sak',
+        phone: '1231234678'
+    }        
+]
+
+
+
+app.get('/',function(request,response){
+
+    // console.log(request.url);
+    return response.render('home',{
+        title: "Contact List!",
+        contact_list: contactList
+    });
+});
+
+app.post('/create-contact',function(request,response){
+    contactList.push({
+        name :request.body.name,
+        phone:request.body.phone
+    })
+    return response.redirect('back');
+});
+
+
 
 app.listen(port, function(err){
     if(err){
